d99f7dbfbbae7e8ce7d0d622dd6201a35365f804
Ivar Lee <ivarr5@gmail.com>
2016-11-16 10:10:40 -0800

    test

--- index.js
+++ index.js
@@ -22,37 +22,37 @@ app.get('/', function(req, res) {
 app.get('/twitch_oauth_endpoint', function(req, res) {
   var code = req.query.code;
 
-  // waterfall([
-  // function(callback){
-  //   var accessToken;
-  //   request.post({
-  //     url:'https://api.twitch.tv/kraken/oauth2/token',
-  //     form: {
-  //           client_id: 'cvmjz4nnl2lh1f30abf9hvgedsg6q6u',
-  //           client_secret: clientSecret,
-  //           grant_type: 'authorization_code',
-  //           redirect_uri: 'https://tweak-game-temp.herokuapp.com/twitch_oauth_endpoint',
-  //           code: code
-  //         }
-  //     }, 
-  //     function(err, httpResponse, body) {
-  //       var body = JSON.parse(body),
-  //       accessToken = body.access_token;
-  //       console.log(body)
-  //       console.log(accessToken + ' first call back');
-  //       callback(null, accessToken);
-  //   });
+  waterfall([
+  function(callback){
+    var accessToken;
+    request.post({
+      url:'https://api.twitch.tv/kraken/oauth2/token',
+      form: {
+            client_id: 'cvmjz4nnl2lh1f30abf9hvgedsg6q6u',
+            client_secret: clientSecret,
+            grant_type: 'authorization_code',
+            redirect_uri: 'https://www.tweak-game-temp.herokuapp.com/twitch_oauth_endpoint',
+            code: code
+          }
+      }, 
+      function(err, httpResponse, body) {
+        var body = JSON.parse(body),
+        accessToken = body.access_token;
+        console.log(body)
+        console.log(accessToken + ' first call back');
+        callback(null, accessToken);
+    });
 
-  // },
-  // function(accessToken, callback) {
-  //     callback(null, accessToken);
-  //   }
-  // ], function (err, result) {
-  //   // result now equals 'done' 
-  //   console.log(result + ' final result');
-  //   var result = result;
-  //   res.render('twitch/twitchEndpoint', {result: result});
-  // });
+  },
+  function(accessToken, callback) {
+      callback(null, accessToken);
+    }
+  ], function (err, result) {
+    // result now equals 'done' 
+    console.log(result + ' final result');
+    var result = result;
+    res.render('twitch/twitchEndpoint', {result: result});
+  });
 
 });
 
