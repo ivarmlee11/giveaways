20c9d38bd6f8f6743f24670e676f8e8ff1773742
Ivar Lee <ivarr5@gmail.com>
2016-11-19 22:50:51 -0800

    fake middleware

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ var express = require('express'),
     console.log(twitchClientId + ' twitch id');
 
 var TwitchtvStrategy = require('passport-twitchtv').Strategy;
-var passport = require('passport')
+var passport = require('passport');
+
 passport.use(new TwitchtvStrategy({
   clientID: twitchClientId,
   clientSecret: twitchClientSecret,
@@ -70,28 +71,38 @@ passport.deserializeUser(function(obj, done) {
 
 app.get('/', function(req, res) {
   res.render('login');
+  console.log(res.locals)
 });
 
 app.get('/auth/twitch', passport.authenticate('twitchtv'));
-
+var user;
 app.get('/auth/twitch/callback', passport.authenticate('twitchtv'), function(req, res) {
   user = req.user;
+  console.log(req.isAuthenticated());
   res.redirect('/auth/twitch/loggedIn');
 });
 
 app.get('/auth/twitch/loggedIn', function(req, res) {
-  if (user.admin) {
-    res.render('makeGame', {user: user});
+  if(user) {
+    if (user.admin) {
+      res.render('makeGame', {user: user});
+    } else {
+      res.redirect('/giveawayList');
+    }
   } else {
-    res.redirect('/giveawayList');
+    res.redirect('/');
   }
 });
 
 app.get('/giveawayList', function(req, res) {
-  db.giveaway.findAll().then(function(giveaways) {
-    var giveaway = giveaways;
-    res.render('giveaways', {giveaways: giveaway});
-  });  
+  if(user) {
+    db.giveaway.findAll().then(function(giveaways) {
+      var giveaway = giveaways;
+      res.render('giveaways', {giveaways: giveaway});
+    });  
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.post('/admin/makeGame', function(req, res) {
@@ -106,83 +117,103 @@ app.post('/admin/makeGame', function(req, res) {
 });
 
 app.get('/admin/adminControl', function(req, res) {
-  db.giveaway.findAll().then(function(giveaways) {
-    var giveaway = giveaways;
-    res.render('adminControl', {giveaways: giveaway});
-  });  
+  if(user) {
+    db.giveaway.findAll().then(function(giveaways) {
+      var giveaway = giveaways;
+      res.render('adminControl', {giveaways: giveaway});
+    });
+  } else {
+    res.redirect('/');
+  }  
 });
 
 app.get('/playerList/:idx', function(req, res) {
+  if(user) {
   var id = req.params.idx;
   db.giveaway.findById(id).then(function(giveaway) {
     var playerList = giveaway.players;
     res.render('showGiveaway', {playerList: playerList});
   });
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.get('/winner/:idx', function(req, res) {
-  var id = req.params.idx;
-  db.giveaway.findById(id).then(function(giveaway) {
-    var playerList = giveaway.players;
-    res.send({playerList: playerList});
-  });
+  if (user) { 
+    var id = req.params.idx;
+    db.giveaway.findById(id).then(function(giveaway) {
+      var playerList = giveaway.players;
+      res.send({playerList: playerList});
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.get('/deleteGiveaway/:idx', function(req, res) {
-  var id = req.params.idx;
-  db.giveaway.destroy({
-    where: { id: id }
-  }).then(function() {
-  });
-  res.redirect('/admin/adminControl');
+  if(user) {
+    var id = req.params.idx;
+    db.giveaway.destroy({
+      where: { id: id }
+    }).then(function() {
+    });
+    res.redirect('/admin/adminControl');
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.get('/giveaway/:idx', function(req,res) {
-  var giveawayId = req.params.idx;  
-  waterfall([
-    function(callback){
-      db.giveaway.find({
-        where: {
-          id: giveawayId
-        }
-      }).then(function(giveaway) {
-        var players = giveaway.players;
-        callback(null, players);
-      });
-    },
-    function(players, callback){
-      var players = players,
-          playerObj = {};
+  if(user) {
+    var giveawayId = req.params.idx;  
+    waterfall([
+      function(callback){
+        db.giveaway.find({
+          where: {
+            id: giveawayId
+          }
+        }).then(function(giveaway) {
+          var players = giveaway.players;
+          callback(null, players);
+        });
+      },
+      function(players, callback){
+        var players = players,
+            playerObj = {};
 
-      if(!players) {
+        if(!players) {
+          players = [];
+        }
+        players.push(user.username);
+
+        players.forEach(function(player) {
+          playerObj[player] = player;
+        });
+
         players = [];
+
+        Object.keys(playerObj).forEach(function(key,index) {
+          players.push(key);
+        });
+
+        db.giveaway.update({
+          players: players
+        }, {
+          where: {
+            id: giveawayId
+          }
+        }).then(function(updatedPlayers) {
+          callback(null, updatedPlayers);
+        });
       }
-      players.push(user.username);
-
-      players.forEach(function(player) {
-        playerObj[player] = player;
-      });
-
-      players = [];
-
-      Object.keys(playerObj).forEach(function(key,index) {
-        players.push(key);
-      });
-
-      db.giveaway.update({
-        players: players
-      }, {
-        where: {
-          id: giveawayId
-        }
-      }).then(function(updatedPlayers) {
-        callback(null, updatedPlayers);
-      });
-    }
-  ],
-  function (err, result) {
-    res.render('thanks');
-  });
+    ],
+    function (err, result) {
+      res.render('thanks');
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.listen(port);
