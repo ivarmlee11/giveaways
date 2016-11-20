760caf5c55478b45bae4325d4fae4e571b14bb08
Ivar Lee <ivarr5@gmail.com>
2016-11-20 01:34:36 -0800

    test

--- index.js
+++ index.js
@@ -9,24 +9,56 @@ var express = require('express'),
     ejsLayouts = require('express-ejs-layouts'),
     twitchClientId = process.env.TWITCHCLIENTID,
     twitchClientSecret = process.env.CLIENTSECRETTWITCH,
-    beamClienetSecret = process.env.CLIENTSECRETBEAM,
+    beamClientSecret = process.env.CLIENTSECRETBEAM,
     beamClientId = process.env.BEAMCLIENTID;
     console.log(twitchClientSecret + ' twitch');
-    console.log(beamClienetSecret + ' beam');
+    console.log(beamClientSecret + ' beam');
     console.log(beamClientId + ' beam id');
     console.log(twitchClientId + ' twitch id');
 
 var TwitchtvStrategy = require('passport-twitchtv').Strategy;
+var BeamStrategy = require('passport-beam').Strategy;
 var passport = require('passport');
 
 passport.use(new TwitchtvStrategy({
   clientID: twitchClientId,
   clientSecret: twitchClientSecret,
-  callbackURL: "http://localhost:3000/auth/twitch/callback",
+  callbackURL: "https://tweak-game-temp.herokuapp.com/auth/twitch/callback",
   scope: "user_read"
 },
   function(accessToken, refreshToken, profile, done) {
     if(profile.username !== ('dridor' || 'tweakgames')) {
+      db.user.findOrCreate({
+        where: {
+          twitchid: profile.id,
+          username: profile.username
+        }
+      }).spread(function(user, created) {
+        return done(null, user);
+      });
+    } else {
+      db.user.findOrCreate({
+        where: {
+          twitchid: profile.id,
+          username: profile.username,
+          admin: true
+        }
+      }).spread(function(user, created) {
+        return done(null, user);
+      });
+    };
+  }
+));
+
+passport.use(new BeamStrategy({
+    clientID: beamClientId,
+    clientSecret: beamClientSecret,
+    callbackURL: "https://tweak-game-temp.herokuapp.com/auth/beam/callback",
+    scope: 'user:details:self'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log('beam strat initiated')
+     if(profile.username !== ('dridor' || 'tweakgames')) {
       db.user.findOrCreate({
         where: {
           twitchid: profile.id,
@@ -71,18 +103,34 @@ passport.deserializeUser(function(obj, done) {
 
 app.get('/', function(req, res) {
   res.render('login');
-  console.log(res.locals)
 });
 
-app.get('/auth/twitch', passport.authenticate('twitchtv'));
 var user;
-app.get('/auth/twitch/callback', passport.authenticate('twitchtv'), function(req, res) {
+
+app.get('/auth/twitch', 
+  passport.authenticate('twitchtv'));
+
+app.get('/auth/twitch/callback', 
+  passport.authenticate('twitchtv',  { failureRedirect: '/' }), 
+  function(req, res) {
   user = req.user;
   console.log(req.isAuthenticated());
-  res.redirect('/auth/twitch/loggedIn');
+  res.redirect('/auth/loggedIn');
 });
 
-app.get('/auth/twitch/loggedIn', function(req, res) {
+app.get('/auth/beam',
+  passport.authenticate('beam'));
+
+app.get('/auth/beam/callback',
+  passport.authenticate('beam', { failureRedirect: '/' }),
+  function(req, res) {
+  user = req.user;
+  console.log(req.isAuthenticated());
+  res.redirect('/auth/loggedIn');
+});
+
+
+app.get('/auth/loggedIn', function(req, res) {
   if(user) {
     if (user.admin) {
       res.render('makeGame', {user: user});
