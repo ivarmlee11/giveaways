76390ed69c85d69ba8e5b684b86b367ebd7ec3ed
Ivar Lee <ivarr5@gmail.com>
2016-11-20 03:21:02 -0800

    test

--- public/js/loginUi.js
+++ public/js/loginUi.js
@@ -1,9 +1,15 @@
 $(function() {
 $('#twitchDiv').hover(function() {
   $('#twitchDiv').css('background-color','silver');
+},
+function() {
+  $('#twitchDiv').css('background-color','white');
 });
 
 $('#beamDiv').hover(function() {
   $('#beamDiv').css('background-color','silver');
+},
+function() {
+  $('#beamDiv').css('background-color','white');
 });
 }); 
