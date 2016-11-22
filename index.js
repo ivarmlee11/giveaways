c22d1f35fd3e59f034cfd933831b9d8311fed4e9
Ivar Lee <ivarr5@gmail.com>
2016-11-22 00:07:25 -0800

    test

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ app.get('/giveawayList', function(req, res) {
 app.get('/giveawayData', function(req, res) {
   db.giveaway.findAll().then(function(giveaways) {
     var giveaway = giveaways;
-    res.send({giveaways: giveaway});
+    res.send(giveaway);
   });  
 });
 
