b57a830c0a6e32d0c9ab4ef41d6fadf2b352a65f
Ivar Lee <ivarr5@gmail.com>
2016-11-22 02:54:56 -0800

    test

--- public/js/addRemoveAdmin.js
+++ public/js/addRemoveAdmin.js
@@ -4,15 +4,13 @@ $('#removeAdmin').on('click', function(e) {
   // e.preventDefault();
   var dataObj = {};
   var adminName = $('#adminNameGive').val();
-  dataObj['adminName'] = adminName;
-
   var selected = $('input[name=auth]:checked').val();
-  console.log(selected);
-  
+  dataObj['adminName'] = adminName;
+  dataObj['auth'] = selected;
   $.ajax({
     method: 'POST',
     url: '/admin/adminListRemove',
-    data: data
+    data: dataObj
   }).done(function(data) {
   });
 });
