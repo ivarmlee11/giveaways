1b1f70b81977d92aa654d504b5972d74199a7ef6
Ivar Lee <ivarr5@gmail.com>
2016-11-21 22:13:49 -0800

    test

--- public/js/addRemoveAdmin.js
+++ public/js/addRemoveAdmin.js
@@ -4,13 +4,13 @@ $('#removeAdmin').on('click', function(e) {
   e.preventDefault();
   var data = $('#adminNameGive').val();
   console.log(data);
+  console.log(typeof data);
 
   $.ajax({
     method: 'POST',
     url: '/admin/adminListRemove',
     data: data
   }).done(function(data) {
-    console.log(data);
   });
 });
 
