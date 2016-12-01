e5b3ea1c6bcf24d899cb8e994466eeb3cd36cd22
Ivar Lee <ivarr5@gmail.com>
2016-12-01 01:19:40 -0800

    testing

--- public/js/liveUpdatePlayerList.js
+++ public/js/liveUpdatePlayerList.js
@@ -5,6 +5,19 @@ $(function() {
         return $(this).attr('giveawayId');
     }).get();
     console.log(giveawayIds);
+    giveawayIds.forEach(function(val) {
+      var url = '/admin/playerListData/' + val;
+      $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(playerList) {
+          console.log(playerList);
+          cosnole.log(playerList.length);
+          $('span[giveawayId=' + val + ']').text('There are ' + playerList.length + ' entries.');
+
+        }
+      });
+    });
   };
 
   getPlayerEntries();
