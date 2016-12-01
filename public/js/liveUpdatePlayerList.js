7ea9fe5f9212215a6b5241d0fa21111d8501388a
Ivar Lee <ivarr5@gmail.com>
2016-12-01 03:52:15 -0800

    update

--- public/js/liveUpdatePlayerList.js
+++ public/js/liveUpdatePlayerList.js
@@ -5,6 +5,7 @@ $(function() {
     var giveawayIds = $('.numberOfPlayer').map( function() {
         return $(this).attr('giveawayId');
     }).get();
+    console.log(giveawayIds);
     giveawayIds.forEach(function(val) {
       var url = '/admin/playerListData/' + val;
       $.ajax({
@@ -12,14 +13,14 @@ $(function() {
         type: 'GET',
         success: function(playerList) {
           $('span[playerNumberId=' + val + ']').text('There are ' + playerList.length + ' entries.');
+          if(playerList.length === 1) {
+            $('span[giveawayId=' + val + ']').text('There is ' + playerList.length + ' entry.');
+          }
           playerList.forEach(function(player) {
             console.log(player)
             console.log('val ' + val)
             $('ul[playerListId=' + val + ']').append('<li>' + player.username + '<img id="logo" src="../img/' + player.auth + '.png" /></li>');
           });
-          if(playerList.length === 1) {
-            $('span[giveawayId=' + val + ']').text('There is ' + playerList.length + ' entry.');
-          }
         }
       });
     });
