269b8edb2012ae1fac95efd16365d018e30c7885
Ivar Lee <ivarr5@gmail.com>
2016-12-01 01:38:53 -0800

    update heroku

--- public/js/liveUpdatePlayerList.js
+++ public/js/liveUpdatePlayerList.js
@@ -11,9 +11,10 @@ $(function() {
         url: url,
         type: 'GET',
         success: function(playerList) {
-          $('span[giveawayId=' + val + ']').text('There are ' + playerList.length + ' entries.');
+          $('span[playerNumberId=' + val + ']').text('There are ' + playerList.length + ' entries.');
           playerList.forEach(function(val) {
-            $('#playersList').append('<li>' + val + '</li>');
+            console.log(val)
+            $('ul[playListId=' + val + ']').append('<li>' + val + '</li>');
           });
           if(playerList.length === 1) {
             $('span[giveawayId=' + val + ']').text('There is ' + playerList.length + ' entry.');
