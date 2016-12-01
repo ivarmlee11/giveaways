263b80aa7872f6d6980fc2ebaa0914ca79b9c0c6
Ivar Lee <ivarr5@gmail.com>
2016-11-30 22:53:52 -0800

    testing

--- controllers/admin.js
+++ controllers/admin.js
@@ -127,7 +127,7 @@ router.get('/playerListData/:idx', ensureAuthenticated, function(req, res) {
           auth: user.auth
         });
       });
-      res.send(playerList.length);
+      res.send(playerList);
     });
   });
 });
