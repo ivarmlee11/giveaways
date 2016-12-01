af83f174f34dd1f12e6621799e2d3517f8940d3d
Ivar Lee <ivarr5@gmail.com>
2016-12-01 13:48:44 -0800

    update

--- public/js/adminLiveUpdatePlayerList.js
+++ public/js/adminLiveUpdatePlayerList.js
@@ -16,9 +16,7 @@ $(function() {
           if(playerList.length === 1) {
             $('span[giveawayId=' + val + ']').text('There is ' + playerList.length + ' entry.');
           }
-          var $listContent = $('ul[playerListId=' + val + ']').html();
-          console.log($listContent);
-          $('ul[playerListId=' + val + ']').html($listContent);
+          $('ul[playerListId=' + val + ']').html('<li></li>');
           playerList.forEach(function(player) {
             console.log(player);
             console.log('val ' + val);
