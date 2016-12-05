bad13f619af418c16cf328fb60fde27983175a79
Ivar Lee <ivarr5@gmail.com>
2016-12-05 15:03:07 -0800

    updating

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ app.use(flash());
 
 app.use(function(req, res, next) {
   res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user.username;
+  if(req.user) {
+    res.locals.currentUser = req.user.username;
+  }
   next();
 });
 
