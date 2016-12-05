6809c014c3eea434f7b688c93749fdcf484c0b54
Ivar Lee <ivarr5@gmail.com>
2016-12-05 15:14:05 -0800

    updating

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ app.use(flash());
 
 app.use(function(req, res, next) {
   res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
+  res.locals.currentUser = req.user.username;
+  console.log('---------------')
+  console.log(req.user);
+    console.log('---------------')
   console.log(res.locals);
   console.log('currentUser middlleware')
   next();
