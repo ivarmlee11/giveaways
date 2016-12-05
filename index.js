e5cb3417cc7a1ee921f043fc37ed0ffc02937cc1
Ivar Lee <ivarr5@gmail.com>
2016-12-05 15:49:23 -0800

    update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(errorhandler());
 
 app.use(function(req, res, next) {
   res.locals.alerts = req.flash();
-  if(req.user.username) {
+  if(req.user) {
     console.log(req.user.username)
     res.locals.currentUser = req.user.username;
   }
