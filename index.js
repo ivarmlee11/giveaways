bed97b92ad8fb63c1d83b3b0d2d6375d9774d16c
Ivar Lee <ivarr5@gmail.com>
2016-12-05 22:20:14 -0800

    update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(errorhandler());
 
 app.use(function(req, res, next) {
   res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user.username;
+  res.locals.currentUser = req.user;
   
   console.log(res.locals);
   console.log('currentUser middlleware')
--- middleware/addLocalVariables.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res, next) {
-  res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
-  console.log(res.locals);
-  console.log('currentUser middlleware')
-  next();
-};
