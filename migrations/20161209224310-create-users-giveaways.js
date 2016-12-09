6f09be8cab384a1536fc7574e1345eb812aed744
Ivar Lee <ivarr5@gmail.com>
2016-12-09 14:44:15 -0800

    update

--- migrations/20161209224310-create-users-giveaways.js
+++ migrations/20161209224310-create-users-giveaways.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      usertId: {
+      userId: {
         type: Sequelize.INTEGER
       },
       giveawayId: {
--- models/usersgiveaways.js
+++ models/usersgiveaways.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var usersGiveaways = sequelize.define('usersGiveaways', {
-    usertId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
     giveawayId: DataTypes.INTEGER
   }, {
     classMethods: {
