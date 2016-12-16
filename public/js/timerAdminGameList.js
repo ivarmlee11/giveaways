560d37cf8526ec4d30da0f7a891bf4bc1f0c36bc
Ivar Lee <ivarr5@gmail.com>
2016-12-16 15:34:04 -0800

    update

--- public/js/timerAdminGameList.js
+++ public/js/timerAdminGameList.js
@@ -12,9 +12,8 @@ $(function() {
   });
   console.log(giveawayIdInts);
   giveawayIdInts.forEach(function(val) {
-    var time = $('h4[giveawayId="' + val + '"]').text();
-    console.log($('h4[giveawayId="' + val + '"]'))
-    console.log(time)
+    var time = $('h4[giveawayId="' + val + '"]').text().split(' ');
+    console.log(time[4]) 
   });
 
 
