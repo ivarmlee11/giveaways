d54f937d4b20e0c3b36a6a863f7bce21e81ca3b6
Ivar Lee <ivarr5@gmail.com>
2016-12-15 18:47:26 -0800

    table updated

--- public/js/adminLiveUpdatePlayerList.js
+++ public/js/adminLiveUpdatePlayerList.js
@@ -89,7 +89,7 @@ var getPlayersandWinners = function(){
       console.log('found winners')
       $('ul[playerListId=' + idx + ']').html('<li>winners are not being added</li>');
       // var updatedWinnerList = newArray(winnerList);
-      updatedWinnerList.forEach(function(player) {
+      winnerList.forEach(function(player) {
         console.log(player + ' winner found');
         $('ul[winnerListId=' + idx + ']').append('<li><strong>' + player.userName + '</strong></span><img id="logo" src="/img/' + player.auth + '.png"/>!</li>');
 
