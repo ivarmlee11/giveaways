73b32859eb462e464f89c6419d8a678ae88c0bc3
Ivar Lee <ivarr5@gmail.com>
2016-12-16 15:23:51 -0800

    update

--- public/js/timerAdminGameList.js
+++ public/js/timerAdminGameList.js
@@ -10,7 +10,7 @@ $(function() {
     var int = parseInt(val);
     giveawayIdInts.push(int);
   });
-
+  console.log(giveawayIdInts);
   giveawayIdInts.forEach(function(val) {
     var time = $('h4[giveawayId="' + val + '"]').val();
     console.log(time)
