2f2b7f498366749bd8b0dff483028571649378cb
Ivar Lee <ivarr5@gmail.com>
2016-12-17 12:09:28 -0800

    update

--- public/js/timerAdminGameList.js
+++ public/js/timerAdminGameList.js
@@ -1,4 +1,20 @@
 $(function() {
+   console.log('timer running')
+
+  var months = {
+    Jan: 01,
+    Feb: 02,
+    Mar: 03,
+    Apr: 04,
+    May: 05,
+    Jun: 06,
+    Jul: 07,
+    Aug: 08,
+    Sep: 09,
+    Oct: 10,
+    Nov: 11,
+    Dec: 12
+  };
 
   var giveawayIds = $('[data="ids"]').map( function() {
     return $(this).attr('giveawayId');
@@ -13,16 +29,19 @@ $(function() {
 
   var contestStartTime;
 
-  console.log('timer running')
 
   console.log(giveawayIds)
 
   giveawayIdInts.forEach(function(val) {
-    var time = $('h4[giveawayId="' + val + '"]').text();
-    var timeSplit = time.split(' ');
-    console.log(timeSplit)
-    contestStartTime = moment.utc(time).format();
-    console.log(contestStartTime + " contest start time")
+    var time = $('h4[giveawayId="' + val + '"]').text(),
+        timeSplit = time.split(' '),
+        month = timeSplit[1],
+        day = timeSplit[2],
+        hourMinSec = timeSplit[4],
+        convertedMonth = month,
+        isoDateString = year + '-' + convertedMonth + '-' + day + 'T' + hourMinSec;
+
+    console.log(isoDateString);
   });
 
 });
