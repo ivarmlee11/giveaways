d0a36f7283d0bf50ee35afef78f827c311fcf405
Ivar Lee <ivarr5@gmail.com>
2016-12-18 16:46:16 -0800

    update

--- public/js/timerAdminGameList.js
+++ public/js/timerAdminGameList.js
@@ -70,7 +70,7 @@ $(function() {
 
 
 
-        console.log(moment.utc(moment(currentTime).diff(moment(startTime))).format("HH:mm:ss"));
+        console.log(moment.utc(moment(currentTime).diff(moment(endTime))).format("HH:mm:ss"));
       }
     });
   });
