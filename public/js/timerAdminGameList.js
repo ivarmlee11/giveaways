2f4d02b3e8d0713d6cf4af6cf96c2c90ec6b83df
Ivar Lee <ivarr5@gmail.com>
2016-12-18 16:59:39 -0800

    update

--- public/js/timerAdminGameList.js
+++ public/js/timerAdminGameList.js
@@ -42,31 +42,35 @@ $(function() {
             startTime = moment(giveawayData.createdAt).utc().format(),
             currentTime = moment.utc().format(),
             endTime;
-        if(giveawayData.timer === 3) {
-          endTime = moment(startTime).add(3, 'minutes').utc().format();
-          var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss");
-          $(timerId).text('The giveaway is on a 3 min timer.');
-          $(timerDisplayId).html(
+        if(!giveawayData.ended) } {
+          if(giveawayData.timer === 3 &&) {
+            endTime = moment(startTime).add(3, 'minutes').utc().format();
+            var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss");
+            $(timerId).text('The giveaway is on a 3 min timer.');
+            $(timerDisplayId).html(
 
-            );
-        } else if(giveawayData.timer === 5) {
-          endTime = moment(startTime).add(5, 'minutes').utc().format();
-          var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss");
-          $(timerId).text('The giveaway is on a 5 min timer.');
-          $(timerDisplayId).html(
+              );
+          } else if(giveawayData.timer === 5) {
+            endTime = moment(startTime).add(5, 'minutes').utc().format();
+            var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss");
+            $(timerId).text('The giveaway is on a 5 min timer.');
+            $(timerDisplayId).html(
 
-            );      
-        } else if(giveawayData.timer === 10) {
-          endTime = moment(startTime).add(10, 'minutes').utc().format(); 
-          var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss");
-          console.log(remainingTime)
-          $(timerId).text('The giveaway is on a 10 min timer.');
-          $(timerDisplayId).html(
+              );      
+          } else if(giveawayData.timer === 10) {
+            endTime = moment(startTime).add(10, 'minutes').utc().format(); 
+            var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss");
+            console.log(remainingTime)
+            $(timerId).text('The giveaway is on a 10 min timer.');
+            $(timerDisplayId).html(
 
-            );   
+              );   
+          } else {
+            endTime = null;
+            $(timerId).text('This giveaway is not timed.');
+          }
         } else {
-          endTime = null;
-          $(timerId).text('This giveaway is not timed.');
+          $(timerId).text('This giveaway has ended.');
         }
         console.log('start time ' + startTime)
         console.log('current time ' + currentTime);
