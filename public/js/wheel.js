1cc3450c8c06f7c4980211943942b3281f4c6f41
Ivar Lee <ivarr5@gmail.com>
2016-12-21 11:51:19 -0800

    update

--- public/js/wheel.js
+++ public/js/wheel.js
@@ -4,27 +4,28 @@ $(function() {
     var venues2 = [];
 
 
-    // var giveawayIds = $('.numberOfPlayer').map( function() {
-    //   return $(this).attr('giveawayId');
-    // }).get();
+    var giveawayIds = $('.numberOfPlayer').map( function() {
+      return $(this).attr('giveawayId');
+    }).get();
 
-    // giveawayIds.forEach(function(element) {
-    //   var url = '/admin/playerListData/' + element;
+    giveawayIds.forEach(function(element) {
+      var url = '/admin/playerListData/' + element;
 
-    //   $.ajax({
-    //     url: url,
-    //     type: 'GET',
-    //     success: function(playerList) {
-    //       playerList.forEach(function(val) {
-    //         venues2.push({
-    //           name: val.username,
-    //           type: 'Player'
-    //         });
-    //       });
-    //       console.log(venues2);
-    //     }
-    //   });
-    // });
+      $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(playerList) {
+          playerList.forEach(function(val) {
+            venues2.push({
+              name: val.username,
+              type: 'Player'
+            });
+          });
+          console.log(venues2);
+          wheel.init();
+        }
+      });
+    });
     
     
     // Helpers
@@ -140,7 +141,7 @@ $(function() {
     init : function(optionList) {
       try {
         wheel.initWheel();
-        wheel.initAudio();
+        // wheel.initAudio();
         wheel.initCanvas();
         wheel.draw();
 
