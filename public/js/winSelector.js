70d455f3795aba5065a133485b6ab6bf73bedbe5
Ivar Lee <ivarr5@gmail.com>
2016-12-21 14:33:04 -0800

    update

--- public/js/winSelector.js
+++ public/js/winSelector.js
@@ -101,285 +101,285 @@ var venues =
     return hash;
   };
 
-  Number.prototype.mod = function(n) {
-    return ((this%n)+n)%n;
-    };
+Number.prototype.mod = function(n) {
+  return ((this%n)+n)%n;
+};
     
-// WHEEL!
-  var wheel = {
-    timerHandle : 0,
-    timerDelay : 33,
+// wheel object
+var wheel = {
+  timerHandle : 0,
+  timerDelay : 33,
 
-    angleCurrent : 0,
-    angleDelta : 0,
+  angleCurrent : 0,
+  angleDelta : 0,
 
-    size : 250,
+  size : 250,
 
-    canvasContext : null,
+  canvasContext : null,
 
-    colors :
-    ['#d14c45',
-    '#d25346',
-    '#d35f47',
-    '#d46849',
-    '#d56e49',
-    '#d77b4b',
-    '#d8874d',
-    '#d98c4d',
-    '#d9914e',
-    '#da9a4f',
-    '#dba150',
-    '#dca951',
-    '#d14c45',
-    '#d58a49',
-    '#c43b33'],
+  colors :
+  ['#d14c45',
+  '#d25346',
+  '#d35f47',
+  '#d46849',
+  '#d56e49',
+  '#d77b4b',
+  '#d8874d',
+  '#d98c4d',
+  '#d9914e',
+  '#da9a4f',
+  '#dba150',
+  '#dca951',
+  '#d14c45',
+  '#d58a49',
+  '#c43b33'],
 
-    segments : [],
+  segments : [],
 
-    seg_colors : [], // Cache of segments to colors
-    
-    maxSpeed : Math.PI / 16,
+  seg_colors : [], // Cache of segments to colors
+  
+  maxSpeed : Math.PI / 16,
 
-    upTime : 3000, // How long to spin up for (in ms)
-    downTime : 10000, // How long to slow down for (in ms)
+  upTime : 3000, // How long to spin up for (in ms)
+  downTime : 10000, // How long to slow down for (in ms)
 
-    spinStart : 0,
+  spinStart : 0,
 
-    frames : 0,
+  frames : 0,
 
-    centerX : 300,
-    centerY : 300,
+  centerX : 300,
+  centerY : 300,
 
-    spin : function() {
-      // Start the wheel only if it's not already spinning
-      if (wheel.timerHandle == 0) {
-        wheel.spinStart = new Date().getTime();
-        wheel.maxSpeed = Math.PI / (16 + Math.random()); // Randomly vary how hard the spin is
-        wheel.frames = 0;
-        // wheel.sound.play();
+  spin : function() {
+    // Start the wheel only if it's not already spinning
+    if (wheel.timerHandle == 0) {
+      wheel.spinStart = new Date().getTime();
+      wheel.maxSpeed = Math.PI / (16 + Math.random()); // Randomly vary how hard the spin is
+      wheel.frames = 0;
+      // wheel.sound.play();
 
-        wheel.timerHandle = setInterval(wheel.onTimerTick, wheel.timerDelay);
-      }
-    },
-
-    onTimerTick : function() {
-            var duration = (new Date().getTime() - wheel.spinStart),
-                progress = 0,
-                finished = false;
-
-      wheel.frames++;
-      wheel.draw();
-
-      if (duration < wheel.upTime) {
-        progress = duration / wheel.upTime;
-        wheel.angleDelta = wheel.maxSpeed
-            * Math.sin(progress * halfPI);
-      } else {
-        progress = duration / wheel.downTime;
-        wheel.angleDelta = wheel.maxSpeed
-            * Math.sin(progress * halfPI + halfPI);
-                if (progress >= 1){
-                    finished = true;
-                }
-      }
-
-      wheel.angleCurrent += wheel.angleDelta;
-            while (wheel.angleCurrent >= doublePI){
-        // Keep the angle in a reasonable range
-        wheel.angleCurrent -= doublePI;
-            }
-      if (finished) {
-        clearInterval(wheel.timerHandle);
-        wheel.timerHandle = 0;
-        wheel.angleDelta = 0;
-
-                if (console){ console.log((wheel.frames / duration * 1000) + " FPS"); }
-      }
-
-      /*
-      // Display RPM
-      var rpm = (wheel.angleDelta * (1000 / wheel.timerDelay) * 60) / (Math.PI * 2);
-      $("#counter").html( Math.round(rpm) + " RPM" );
-       */
-    },
-
-    init : function(optionList) {
-      try {
-        wheel.initWheel();
-        // wheel.initAudio();
-        wheel.initCanvas();
-        wheel.draw();
-
-        $.extend(wheel, optionList);
-
-      } catch (exceptionData) {
-        alert('Wheel is not loaded ' + exceptionData);
-      }
-
-    },
-
-    // initAudio : function() {
-    //   var sound = document.createElement('audio');
-    //   sound.setAttribute('src', 'wheel.mp3');
-    //   wheel.sound = sound;
-    // },
-
-    initCanvas : function() {
-      var canvas = $('#canvas')[0];
-      canvas.addEventListener("click", wheel.spin, false);
-      wheel.canvasContext = canvas.getContext("2d");
-    },
-
-    initWheel : function() {
-      shuffle(wheel.colors);
-    },
-
-    // Called when segments have changed
-    update : function() {
-      // Ensure we start mid way on a item
-      //var r = Math.floor(Math.random() * wheel.segments.length);
-      var r = 0,
-          segments = wheel.segments,
-          len = segments.length,
-          colors = wheel.colors,
-          colorLen = colors.length,
-          seg_color = [], // Generate a color cache (so we have consistant coloring)
-                i
-            wheel.angleCurrent = ((r + 0.5) / wheel.segments.length) * doublePI;
-            
-            for (i = 0; i < len; i++){
-        seg_color.push( colors [ segments[i].hashCode().mod(colorLen) ] );
-            }
-      wheel.seg_color = seg_color;
-
-      wheel.draw();
-    },
-
-    draw : function() {
-      wheel.clear();
-      wheel.drawWheel();
-      wheel.drawNeedle();
-    },
-
-    clear : function() {
-      wheel.canvasContext.clearRect(0, 0, 1000, 800);
-    },
-
-    drawNeedle : function() {
-      var ctx = wheel.canvasContext,
-                centerX = wheel.centerX,
-                centerY = wheel.centerY,
-                size = wheel.size,
-                i,
-                centerSize = centerX + size,
-                len = wheel.segments.length,
-                winner;
-
-      ctx.lineWidth = 2;
-      ctx.strokeStyle = blackHex;
-      ctx.fillStyle = whiteHex;
-
-      ctx.beginPath();
-
-      ctx.moveTo(centerSize - 10, centerY);
-      ctx.lineTo(centerSize + 10, centerY - 10);
-      ctx.lineTo(centerSize + 10, centerY + 10);
-      ctx.closePath();
-
-      ctx.stroke();
-      ctx.fill();
-
-      // Which segment is being pointed to?
-      i = len - Math.floor((wheel.angleCurrent / doublePI) * len) - 1;
-
-      // Now draw the winning name
-      ctx.textAlign = "left";
-      ctx.textBaseline = "middle";
-      ctx.fillStyle = blackHex;
-      ctx.font = "2em Lato";
-      winner = wheel.segments[i] || 'Choose at least 1 Player';
-      winnerReset = true;
-      $('#winner').html('The winner is ' + winner + '!');
-      ctx.fillText(winner, centerSize + 20, centerY);
-    },
-
-    drawSegment : function(key, lastAngle, angle) {
-      var ctx = wheel.canvasContext,
-                centerX = wheel.centerX,
-                centerY = wheel.centerY,
-                size = wheel.size,
-                colors = wheel.seg_color,
-                value = wheel.segments[key];
-      
-      //ctx.save();
-      ctx.beginPath();
-
-      // Start in the centre
-      ctx.moveTo(centerX, centerY);
-      ctx.arc(centerX, centerY, size, lastAngle, angle, false); // Draw an arc around the edge
-      ctx.lineTo(centerX, centerY); // Now draw a line back to the centre
-
-      // Clip anything that follows to this area
-      //ctx.clip(); // It would be best to clip, but we can double performance without it
-      ctx.closePath();
-
-      ctx.fillStyle = colors[key];
-      ctx.fill();
-      ctx.stroke();
-
-      // Now draw the text
-      ctx.save(); // The save ensures this works on Android devices
-      ctx.translate(centerX, centerY);
-      ctx.rotate((lastAngle + angle) / 2);
-
-      ctx.fillStyle = whiteHex;
-      ctx.fillText(value.substr(0, 20), size-15, 0);
-      ctx.restore();
-    },
-
-    drawWheel : function() {
-      var ctx = wheel.canvasContext,
-                angleCurrent = wheel.angleCurrent,
-                lastAngle    = angleCurrent,
-                len       = wheel.segments.length,
-                centerX = wheel.centerX,
-                centerY = wheel.centerY,
-                size    = wheel.size,
-                angle,
-                i;
-
-      ctx.lineWidth    = 1;
-      ctx.strokeStyle  = blackHex;
-      ctx.textBaseline = "middle";
-      ctx.textAlign    = "right";
-      ctx.font         = "1em Lato";
-
-      for (i = 1; i <= len; i++) {
-        angle = doublePI * (i / len) + angleCurrent;
-        wheel.drawSegment(i - 1, lastAngle, angle);
-        lastAngle = angle;
-      }
-            
-      // Draw a center circle
-      ctx.beginPath();
-      ctx.arc(centerX, centerY, 20, 0, doublePI, false);
-      ctx.closePath();
-
-      ctx.fillStyle   = whiteHex;
-      //ctx.strokeStyle = blackHex;
-      ctx.fill();
-      ctx.stroke();
-
-      // Draw outer circle
-      ctx.beginPath();
-      ctx.arc(centerX, centerY, size, 0, doublePI, false);
-      ctx.closePath();
-
-      ctx.lineWidth   = 2;
-      // ctx.strokeStyle = blackHex;
-      ctx.stroke();
+      wheel.timerHandle = setInterval(wheel.onTimerTick, wheel.timerDelay);
     }
-  };
+  },
+
+  onTimerTick : function() {
+          var duration = (new Date().getTime() - wheel.spinStart),
+              progress = 0,
+              finished = false;
+
+    wheel.frames++;
+    wheel.draw();
+
+    if (duration < wheel.upTime) {
+      progress = duration / wheel.upTime;
+      wheel.angleDelta = wheel.maxSpeed
+          * Math.sin(progress * halfPI);
+    } else {
+      progress = duration / wheel.downTime;
+      wheel.angleDelta = wheel.maxSpeed
+          * Math.sin(progress * halfPI + halfPI);
+              if (progress >= 1){
+                  finished = true;
+              }
+    }
+
+    wheel.angleCurrent += wheel.angleDelta;
+          while (wheel.angleCurrent >= doublePI){
+      // Keep the angle in a reasonable range
+      wheel.angleCurrent -= doublePI;
+          }
+    if (finished) {
+      clearInterval(wheel.timerHandle);
+      wheel.timerHandle = 0;
+      wheel.angleDelta = 0;
+
+              if (console){ console.log((wheel.frames / duration * 1000) + " FPS"); }
+    }
+
+    /*
+    // Display RPM
+    var rpm = (wheel.angleDelta * (1000 / wheel.timerDelay) * 60) / (Math.PI * 2);
+    $("#counter").html( Math.round(rpm) + " RPM" );
+     */
+  },
+
+  init : function(optionList) {
+    try {
+      wheel.initWheel();
+      // wheel.initAudio();
+      wheel.initCanvas();
+      wheel.draw();
+
+      $.extend(wheel, optionList);
+
+    } catch (exceptionData) {
+      alert('Wheel is not loaded ' + exceptionData);
+    }
+
+  },
+
+  // initAudio : function() {
+  //   var sound = document.createElement('audio');
+  //   sound.setAttribute('src', 'wheel.mp3');
+  //   wheel.sound = sound;
+  // },
+
+  initCanvas : function() {
+    var canvas = $('#canvas')[0];
+    canvas.addEventListener("click", wheel.spin, false);
+    wheel.canvasContext = canvas.getContext("2d");
+  },
+
+  initWheel : function() {
+    shuffle(wheel.colors);
+  },
+
+  // Called when segments have changed
+  update : function() {
+    // Ensure we start mid way on a item
+    //var r = Math.floor(Math.random() * wheel.segments.length);
+    var r = 0,
+        segments = wheel.segments,
+        len = segments.length,
+        colors = wheel.colors,
+        colorLen = colors.length,
+        seg_color = [], // Generate a color cache (so we have consistant coloring)
+              i
+          wheel.angleCurrent = ((r + 0.5) / wheel.segments.length) * doublePI;
+          
+          for (i = 0; i < len; i++){
+      seg_color.push( colors [ segments[i].hashCode().mod(colorLen) ] );
+          }
+    wheel.seg_color = seg_color;
+
+    wheel.draw();
+  },
+
+  draw : function() {
+    wheel.clear();
+    wheel.drawWheel();
+    wheel.drawNeedle();
+  },
+
+  clear : function() {
+    wheel.canvasContext.clearRect(0, 0, 1000, 800);
+  },
+
+  drawNeedle : function() {
+    var ctx = wheel.canvasContext,
+              centerX = wheel.centerX,
+              centerY = wheel.centerY,
+              size = wheel.size,
+              i,
+              centerSize = centerX + size,
+              len = wheel.segments.length,
+              winner;
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = blackHex;
+    ctx.fillStyle = whiteHex;
+
+    ctx.beginPath();
+
+    ctx.moveTo(centerSize - 10, centerY);
+    ctx.lineTo(centerSize + 10, centerY - 10);
+    ctx.lineTo(centerSize + 10, centerY + 10);
+    ctx.closePath();
+
+    ctx.stroke();
+    ctx.fill();
+
+    // Which segment is being pointed to?
+    i = len - Math.floor((wheel.angleCurrent / doublePI) * len) - 1;
+
+    // Now draw the winning name
+    ctx.textAlign = "left";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = blackHex;
+    ctx.font = "2em Lato";
+    winner = wheel.segments[i] || '... Nobody has entered.';
+    winnerReset = true;
+    $('#winner').html('The winner is ' + winner + '!');
+    ctx.fillText(winner, centerSize + 20, centerY);
+  },
+
+  drawSegment : function(key, lastAngle, angle) {
+    var ctx = wheel.canvasContext,
+              centerX = wheel.centerX,
+              centerY = wheel.centerY,
+              size = wheel.size,
+              colors = wheel.seg_color,
+              value = wheel.segments[key];
+    
+    //ctx.save();
+    ctx.beginPath();
+
+    // Start in the centre
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, size, lastAngle, angle, false); // Draw an arc around the edge
+    ctx.lineTo(centerX, centerY); // Now draw a line back to the centre
+
+    // Clip anything that follows to this area
+    //ctx.clip(); // It would be best to clip, but we can double performance without it
+    ctx.closePath();
+
+    ctx.fillStyle = colors[key];
+    ctx.fill();
+    ctx.stroke();
+
+    // Now draw the text
+    ctx.save(); // The save ensures this works on Android devices
+    ctx.translate(centerX, centerY);
+    ctx.rotate((lastAngle + angle) / 2);
+
+    ctx.fillStyle = whiteHex;
+    ctx.fillText(value.substr(0, 20), size-15, 0);
+    ctx.restore();
+  },
+
+  drawWheel : function() {
+    var ctx = wheel.canvasContext,
+              angleCurrent = wheel.angleCurrent,
+              lastAngle    = angleCurrent,
+              len       = wheel.segments.length,
+              centerX = wheel.centerX,
+              centerY = wheel.centerY,
+              size    = wheel.size,
+              angle,
+              i;
+
+    ctx.lineWidth    = 1;
+    ctx.strokeStyle  = blackHex;
+    ctx.textBaseline = "middle";
+    ctx.textAlign    = "right";
+    ctx.font         = "1em Lato";
+
+    for (i = 1; i <= len; i++) {
+      angle = doublePI * (i / len) + angleCurrent;
+      wheel.drawSegment(i - 1, lastAngle, angle);
+      lastAngle = angle;
+    }
+          
+    // Draw a center circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 20, 0, doublePI, false);
+    ctx.closePath();
+
+    ctx.fillStyle   = whiteHex;
+    //ctx.strokeStyle = blackHex;
+    ctx.fill();
+    ctx.stroke();
+
+    // Draw outer circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, size, 0, doublePI, false);
+    ctx.closePath();
+
+    ctx.lineWidth   = 2;
+    // ctx.strokeStyle = blackHex;
+    ctx.stroke();
+  }
+};
 
 
 
