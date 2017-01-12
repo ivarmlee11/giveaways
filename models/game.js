67b33bbcc7cc8e3ac9988f585b8d1a958d81eeb3
Ivar Lee <ivarr5@gmail.com>
2017-01-12 12:33:29 -0800

    update

--- models/game.js
+++ models/game.js
@@ -1,5 +1,4 @@
 'use strict';
-var bcrypt = require('bcrypt');
 
 module.exports = function(sequelize, DataTypes) {
   var game = sequelize.define('game', {
@@ -12,15 +11,6 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-      }
-    },
-    hooks: {
-      beforeCreate: function(game, options, cb) {
-        game.generateHash(game.code, function(err, encrypted){
-          if (err) return next(err);
-          game.code = encrypted;
-          cb(null, game);
-        });
       }
     }
   });
