a6ad1b824c2dc78ba8175db22fe3b065620c4b6c
Ivar Lee <ivarr5@gmail.com>
2017-01-12 08:16:25 -0800

    update

--- public/js/selectPlayerWinner.js
+++ public/js/selectPlayerWinner.js
@@ -98,7 +98,7 @@ $('#addWinnerToDb').on('click', function() {
 
     if($('#saveGameToggle').is(":checked") && afterFirstSpinWheel) {
       var url = '/game/winnerCard';
-      game.userId = $('#winnerId').html();
+      // game.userId = $('#winnerId').html();
       console.log(game)
       $.ajax({
         url: url,
