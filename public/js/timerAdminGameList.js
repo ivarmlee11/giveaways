6fcc087998b55e5e6ff95a80ebd40b6c307e0415
Ivar Lee <ivarr5@gmail.com>
2017-01-12 14:26:05 -0800

    update

--- public/js/timerAdminGameList.js
+++ public/js/timerAdminGameList.js
@@ -23,12 +23,11 @@ var contestStartTime;
 
 var giveawayIdInts = [];
 
-var diffInSeconds = function(string) {
+function diffInSeconds(string) {
   var splitString = string.split(':'),
       mins = parseInt(splitString[0]),
       seconds = parseInt(splitString[1]),
       totalSeconds = (mins * 60) + seconds;
-  console.log(totalSeconds);
   return totalSeconds;
 }
 
@@ -37,80 +36,81 @@ giveawayIds.forEach(function(val) {
   giveawayIdInts.push(int);
 });
 
-giveawayIdInts.forEach(function(val) {
+function remakeTimer(){
+  giveawayIdInts.forEach(function(val) {
 
-  var url = '/giveaway/giveawayData/' + val;
+    var url = '/giveaway/giveawayData/' + val;
 
-  $.ajax({
-    url: url,
-    type: 'GET',
-    success: function(giveawayData) {
-      var timerId = '#timer' + val,
-          timerDisplayId = '#timerDisplay' + val,
-          startTime = moment(giveawayData.createdAt).utc().format(),
-          currentTime = moment.utc().format(),
-          endTime;
-      if(!giveawayData.ended) {
-        if(giveawayData.timer === 3) {
-          endTime = moment(startTime).add(3, 'minutes').utc().format();
-          var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss"),
-              timerSeconds = diffInSeconds(remainingTime),
-              timerId = 'clock' + val,
-              timerIdTag = '#' + timerId,
-              timerString =  '<div id="' + timerId + '" data-timer="' + timerSeconds + '"></div>';
+    $.ajax({
+      url: url,
+      type: 'GET',
+      success: function(giveawayData) {
+        var timerId = '#timer' + val,
+            timerDisplayId = '#timerDisplay' + val,
+            startTime = moment(giveawayData.createdAt).utc().format(),
+            currentTime = moment.utc().format(),
+            endTime;
+        if(!giveawayData.ended) {
+          if(giveawayData.timer === 3) {
+            endTime = moment(startTime).add(3, 'minutes').utc().format();
+            var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss"),
+                timerSeconds = diffInSeconds(remainingTime),
+                timerId = 'clock' + val,
+                timerIdTag = '#' + timerId,
+                timerString =  '<div id="' + timerId + '" data-timer="' + timerSeconds + '"></div>';
 
-          $(timerId).text('The giveaway is on a 3 min timer.');
-          $(timerDisplayId).html(timerString);
-          if(moment(currentTime).isBefore(endTime, 'seconds')) {
-            $(timerIdTag).TimeCircles({count_past_zero: false});
+            $(timerId).text('The giveaway is on a 3 min timer.');
+            $(timerDisplayId).html(timerString);
+            if(moment(currentTime).isBefore(endTime, 'seconds')) {
+              $(timerIdTag).TimeCircles({count_past_zero: false});
+            } else {
+              $(timerIdTag).text('Giveaway is over.');
+            }
+          } else if(giveawayData.timer === 5) {
+            endTime = moment(startTime).add(5, 'minutes').utc().format();
+            var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss"),
+                timerSeconds = diffInSeconds(remainingTime),
+                timerId = 'clock' + val,
+                timerIdTag = '#' + timerId,
+                timerString =  '<div id="' + timerId + '" data-timer="' + timerSeconds + '"></div>';
+
+            $(timerId).text('The giveaway is on a 5 min timer.');
+            $(timerDisplayId).html(timerString);
+            if(moment(currentTime).isBefore(endTime, 'seconds')) {
+              $(timerIdTag).TimeCircles({count_past_zero: false});
+            } else {
+              $(timerIdTag).text('Giveaway is over.');
+            }
+          } else if(giveawayData.timer === 10) {
+            endTime = moment(startTime).add(10, 'minutes').utc().format();
+            var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss"),
+                timerSeconds = diffInSeconds(remainingTime),
+                timerId = 'clock' + val,
+                timerIdTag = '#' + timerId,
+                timerString =  '<div id="' + timerId + '" data-timer="' + timerSeconds + '"></div>';
+
+            $(timerId).text('The giveaway is on a 10 min timer.');
+            $(timerDisplayId).html(timerString);
+            if(moment(currentTime).isBefore(endTime, 'seconds')) {
+              $(timerIdTag).TimeCircles({count_past_zero: false});
+            } else {
+              $(timerIdTag).text('Giveaway is over.');
+            }
           } else {
-            $(timerIdTag).text('Giveaway is over.');
-          }
-        } else if(giveawayData.timer === 5) {
-          endTime = moment(startTime).add(5, 'minutes').utc().format();
-          var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss"),
-              timerSeconds = diffInSeconds(remainingTime),
-              timerId = 'clock' + val,
-              timerIdTag = '#' + timerId,
-              timerString =  '<div id="' + timerId + '" data-timer="' + timerSeconds + '"></div>';
-
-          $(timerId).text('The giveaway is on a 5 min timer.');
-          $(timerDisplayId).html(timerString);
-          if(moment(currentTime).isBefore(endTime, 'seconds')) {
-            $(timerIdTag).TimeCircles({count_past_zero: false});
-          } else {
-            $(timerIdTag).text('Giveaway is over.');
-          }
-        } else if(giveawayData.timer === 10) {
-          endTime = moment(startTime).add(10, 'minutes').utc().format();
-          var remainingTime = moment.utc(moment(endTime).diff(moment(currentTime))).format("mm:ss"),
-              timerSeconds = diffInSeconds(remainingTime),
-              timerId = 'clock' + val,
-              timerIdTag = '#' + timerId,
-              timerString =  '<div id="' + timerId + '" data-timer="' + timerSeconds + '"></div>';
-
-          $(timerId).text('The giveaway is on a 10 min timer.');
-          $(timerDisplayId).html(timerString);
-          if(moment(currentTime).isBefore(endTime, 'seconds')) {
-            $(timerIdTag).TimeCircles({count_past_zero: false});
-          } else {
-            $(timerIdTag).text('Giveaway is over.');
+            endTime = null;
+            $(timerId).text('This giveaway is not timed.');
           }
         } else {
-          endTime = null;
-          $(timerId).text('This giveaway is not timed.');
+          $(timerId).text('This giveaway has ended.');
         }
-      } else {
-        $(timerId).text('This giveaway has ended.');
       }
-    }
+    });
   });
-});
+}
 
 
 window.onresize = function(event) {
-  $('[data=display]').html('');
-  $('[data=display]').TimeCircles().rebuild();   
+   remakeTimer();  
 };
 
 
