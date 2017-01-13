01e3df532bfcf0bd0b61cc7f0116a12383982b6a
Ivar Lee <ivarr5@gmail.com>
2017-01-12 16:06:37 -0800

    update

--- public/js/tradingCards.js
+++ public/js/tradingCards.js
@@ -6,19 +6,20 @@ $(function() {
 
   function updateCards() {
     var url = '/game/winnerCard/'
-   $.ajax({
-    url: url,
-    type: 'GET',
-    success: function(cardList) {
-      cardList.forEach(function(val) {
-        console.log(val);
-        $tradingArea.append('<div gameId="' + val.id + '" class="cards">' + 
-          '<h3>' + val.name + '</h3>' + 
-          '</div>'
-        )
-      })
-    }
-  });
-  updateCards();
+     $.ajax({
+      url: url,
+      type: 'GET',
+      success: function(cardList) {
+        cardList.forEach(function(val) {
+          console.log(val);
+          $tradingArea.append('<div gameId="' + val.id + '" class="cards">' + 
+            '<h3>' + val.name + '</h3>' + 
+            '</div>'
+          )
+        })
+      }
+    });
   };
+
+  updateCards();
 });
