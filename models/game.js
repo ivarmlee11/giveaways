495efd86870746836c7520adea16f4e419a04d49
Ivar Lee <ivarr5@gmail.com>
2017-01-14 03:24:56 -0800

    update

--- models/game.js
+++ models/game.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        models.game.belongsToMany(models.user, {as: 'Users', through: 'gamesUsers'});
+        models.game.belongsTo(models.user, {as: 'Users', through: 'gamesUsers'});
       }
     }
   });
