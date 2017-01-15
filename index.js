b4db8ef0f91f654f62422efc4a5433b9162857ae
Ivar Lee <ivarr5@gmail.com>
2017-01-15 06:30:37 -0800

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var express = require('express'),
     requestIp = require('request-ip'),
     tmi = require('tmi.js'),
     botKey = process.env.BOTAPIKEY,
-    io = require('socket.io')(),
+    io = require('socket.io')(app),
     sharedsession = require('express-socket.io-session'),
     flash = require('connect-flash');
  
