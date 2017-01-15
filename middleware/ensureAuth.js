05190552fedcf231d5a35d83f7e6e3acfbce6a21
Ivar Lee <ivarr5@gmail.com>
2017-01-15 05:46:31 -0800

    update

--- middleware/ensureAuth.js
+++ middleware/ensureAuth.js
@@ -1,6 +1,7 @@
 module.exports = function (req, res, next) {
   if (req.isAuthenticated()) {
     console.log(req.user.username + ' user entered a route.');
+    console.log(req.session)
     return next(); 
   };
   res.redirect('/')
