ec64bec9046d46e510fe5c3bb37741411bd3c6f7
Ivar Lee <ivarr5@gmail.com>
2017-01-15 14:34:04 -0800

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var express = require('express'),
     io = require("socket.io")(server),
     flash = require('connect-flash'),
     sharedsession = require("express-socket.io-session");
-server.listen(80);
+server.listen(app.listen(port));
 app.use(requestIp.mw());
 
 app.use(cookieParser());
