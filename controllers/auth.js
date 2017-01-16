506bed8197f78203ea257a2b383efeedefac64d1
Ivar Lee <ivarr5@gmail.com>
2017-01-16 08:04:21 -0800

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,7 @@ router.get('/beam/callback',
 router.get('/loggedIn', ensureAuthenticated, function(req, res) {
   if (req.user.admin) {
     req.flash('success', 'Admin logged in.');
-    res.redirect('/giveawayList');
+    res.render('admin/adminGameList');
   } else {
     req.flash('success', 'Welcome back.');
     res.redirect('/giveawayList');
