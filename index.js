e0beed3d14c5faeceae524db1d51d39ca8df0d79
Ivar Lee <ivarr5@gmail.com>
2017-01-16 05:13:13 -0800

    update

--- index.js
+++ index.js
@@ -113,6 +113,7 @@ client.on("join", function (channel, username, self) {
 });
 
 io.on('connection', function (socket) {
+  console.log(socket)
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
