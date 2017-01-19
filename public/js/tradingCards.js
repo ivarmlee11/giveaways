b4b41ad3696388e55bd57acf620b1f6027b9984c
Ivar Lee <ivarr5@gmail.com>
2017-01-19 11:14:19 -0800

    update

--- public/js/tradingCards.js
+++ public/js/tradingCards.js
@@ -54,7 +54,7 @@ socket.on('get trade', function(trade) {
       sentToId: trade.sentFromId
     };
     socket.emit('Trade in progress', message);
-  } else if (tradeInProgress && !trade.clearThis && (trade.sentFromId === tradeInfoIn.sentFromId)) {
+  } else if (!trade.clearThis && (trade.sentFromId === tradeInfoIn.sentFromId)) {
     tradeInfoIn = trade;
     gameListIn.html(tradeInfoIn.gameId.length + ' items');
     messageBox.html(tradeInfoIn.sentFromName + ' has updated the trade proposal.');
