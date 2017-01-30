f782bf7a99f98dd1e4eaa7b0d2828bba9be1827f
Ivar Lee <ivarr5@gmail.com>
2017-01-30 09:05:09 -0800

    update

--- public/js/tradingCards.js
+++ public/js/tradingCards.js
@@ -68,7 +68,7 @@ socket.on('get trade', function(trade) {
     gameListIn.html(tradeInfoIn.gameId.length + ' items')
     messageBox.html('Incoming trade arrived from ' + tradeInfoIn.sentFromName)
     tradeInProgressIndicator.html('Trade in progress')
-  } else if (tradeInProgress &&  (trade.userId === tradeInfoOut.userId)) {
+  } else if (tradeInProgress &&  (trade.sentFromId === tradeInfoOut.userId)) {
     console.log('trade busy')
     var message = { 
       message: 'That trader has a trade in progress',
