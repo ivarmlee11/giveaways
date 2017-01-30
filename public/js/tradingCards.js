518e15aed1039907766fe4838e8d0b9edff21b77
Ivar Lee <ivarr5@gmail.com>
2017-01-30 12:28:19 -0800

    update

--- public/js/tradingCards.js
+++ public/js/tradingCards.js
@@ -106,6 +106,7 @@ socket.on('get trade', function(trade) {
     tradeInfoIn.gameId = []
     tradeInfoOut.gameId = []
     tradeInProgress = false
+    
     tradeInProgressIndicator.html('Trade not in progress')
     messageBox.html('The other trade reset the trade')
   }
