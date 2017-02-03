7481240ed9c2110e7362d941bdcd23f364726591
Ivar Lee <ivarr5@gmail.com>
2017-02-03 13:34:40 -0800

    update

--- public/js/uploadGames.js
+++ public/js/uploadGames.js
@@ -7,18 +7,18 @@ var form = $('#uploadForm'),
 form.submit(function(){
 
 var formData = new FormData($(this)[0]);
-
-$.ajax({
-    url: '/game/uploadGameData',
-    type: 'POST',
-    data: formData,
-    async: false,
-    success: function (data) {
-    },
-    cache: false,
-    contentType: false,
-    processData: false
-});
+console.log(formData)
+// $.ajax({
+//     url: '/game/uploadGameData',
+//     type: 'POST',
+//     data: formData,
+//     async: false,
+//     success: function (data) {
+//     },
+//     cache: false,
+//     contentType: false,
+//     processData: false
+// });
 
 });
 
