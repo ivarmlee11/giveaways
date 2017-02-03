db32c5fb5e65a413daea674be72e269e4c565ce3
Ivar Lee <ivarr5@gmail.com>
2017-02-03 14:52:09 -0800

    update

--- public/js/uploadGames.js
+++ public/js/uploadGames.js
@@ -8,20 +8,25 @@ form.submit(function(e){
 
 e.preventDefault();
 
-var formData = new FormData(form[0]);
-console.log(formData)
-// $.ajax({
-//     url: '/game/uploadGameData',
-//     type: 'POST',
-//     data: formData,
-//     async: false,
-//     success: function (data) {
-//     },
-//     cache: false,
-//     contentType: false,
-//     processData: false
-// });
-
+$(this).parse({
+  config: {
+    // base config to use for each file
+  },
+  before: function(file, inputElem)
+  {
+    // executed before parsing each file begins;
+    // what you return here controls the flow
+  },
+  error: function(err, file, inputElem, reason)
+  {
+    // executed if an error occurs while loading the file,
+    // or if before callback aborted for some reason
+  },
+  complete: function(file)
+  {
+    // executed after all files are complete
+    console.log(file)
+  }
 });
 
 })
