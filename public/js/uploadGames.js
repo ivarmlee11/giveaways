5bb0b74553de1836504c8f6733a5a543a19244f0
Ivar Lee <ivarr5@gmail.com>
2017-02-03 15:12:53 -0800

    update

--- public/js/uploadGames.js
+++ public/js/uploadGames.js
@@ -8,25 +8,11 @@ form.submit(function(e){
 
 e.preventDefault();
 
-$(this).parse({
-  config: {
-    // base config to use for each file
-  },
-  before: function(file, inputElem)
-  {
-    // executed before parsing each file begins;
-    // what you return here controls the flow
-  },
-  error: function(err, file, inputElem, reason)
-  {
-    // executed if an error occurs while loading the file,
-    // or if before callback aborted for some reason
-  },
-  complete: function(file)
-  {
-    // executed after all files are complete
-    console.log(file)
+Papa.parse(uploadGameData.files[0], {
+  complete: function(results) {
+    console.log(results);
   }
 });
+
 });
 })
