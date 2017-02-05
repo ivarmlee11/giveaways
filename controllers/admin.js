c9614bc44da18cbb252f0e1332ac8c4859ca354a
Ivar Lee <ivarr5@gmail.com>
2017-02-04 19:13:42 -0800

    update

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,11 +4,11 @@ var express = require('express'),
     ensureAuthenticated = require('../middleware/ensureAuth.js'),
     db = require('../models'),
     moment = require('moment-timezone'),
-    flash = require('connect-flash');
+    flash = require('connect-flash')
 
 router.get('/makeAGiveaway', ensureAuthenticated, modCheck, function(req, res) {
-  res.render('admin/adminControl');
-});
+  res.render('admin/adminControl')
+})
 
 router.get('/adminList', ensureAuthenticated, modCheck, function(req, res) {
   db.user.findAll({
@@ -16,10 +16,10 @@ router.get('/adminList', ensureAuthenticated, modCheck, function(req, res) {
       admin: true
     }
   }).then(function(allAdmins) {
-    var allAdmins = allAdmins;
-    res.render('admin/adminList', {allAdmins: allAdmins});
-  });
-});
+    var allAdmins = allAdmins
+    res.render('admin/adminList', {allAdmins: allAdmins})
+  })
+})
 
 router.post('/adminListAdd', ensureAuthenticated, modCheck, function(req, res) {
   db.user.update({
@@ -30,22 +30,22 @@ router.post('/adminListAdd', ensureAuthenticated, modCheck, function(req, res) {
       auth: req.body.auth
     }
   }).then(function(user) {
-    req.flash('success', 'Admin added.');
-    res.redirect('/admin/adminList');
-  });
-});
+    req.flash('success', 'Admin added.')
+    res.redirect('/admin/adminList')
+  })
+})
 
 router.post('/adminListRemove', ensureAuthenticated, modCheck, function(req, res) {
   var adminName = req.body.adminName,
-      auth = req.body.auth;
+      auth = req.body.auth
 
-  console.log(adminName + ' admin name');
-  console.log(req.user.username + ' req.user.username');
+  console.log(adminName + ' admin name')
+  console.log(req.user.username + ' req.user.username')
 
   if(adminName === req.user.username) {
     console.log('your names are the same, you cannot demod yaself')
-    req.flash('info', 'You cannot demod yourself.');
-    res.redirect('/admin/adminList');
+    req.flash('info', 'You cannot demod yourself.')
+    res.redirect('/admin/adminList')
   } else {
     db.user.update({
       admin: false
@@ -55,10 +55,10 @@ router.post('/adminListRemove', ensureAuthenticated, modCheck, function(req, res
         auth: auth
       }
     }).then(function(user) {
-      req.flash('success', 'You removed admin status.');
-      res.redirect('/admin/adminList');
-    });
+      req.flash('success', 'You removed admin status.')
+      res.redirect('/admin/adminList')
+    })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
