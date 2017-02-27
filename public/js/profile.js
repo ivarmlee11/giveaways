5f9d00ebbb28bb6ebc46b55b6e49ef839387d03d
ivarmlee1 <ivarr5@gmail.com>
2017-02-27 03:54:50 -0800

    updating heroku

--- public/js/profile.js
+++ public/js/profile.js
@@ -14,7 +14,7 @@ $.ajax({
     console.log(profilePic.length)
   	if(profilePic === "") {
   		$thumbNail.html('<img src="../img/guesswho.png"/>')
-      $thumbMessage.htmk('No thumbnail change submitted yet. Upload an image.')
+      $thumbMessage.html('No thumbnail change submitted yet. Upload an image.')
     } else if (profilePic === false) {
       $thumbNail.html('<img src="../img/guesswho.png"/>')
       $thumbMessage.html('The last thumbnail you suggested was rejected.')
