6d3ee176b00287a6589fd2a799a982fff7c3a8cb
ivarmlee1 <ivarr5@gmail.com>
2017-03-21 10:34:50 -0700

    updating heroku

--- controllers/auction.js
+++ controllers/auction.js
@@ -83,7 +83,7 @@ router.post('/viewerAuction/bid', ensureAuthenticated, function(req, res) {
                   owned: true
                 }, {
                   where: {
-                    id: gameListId
+                    id: gameId
                   }
                 })
                 .then(function() {
