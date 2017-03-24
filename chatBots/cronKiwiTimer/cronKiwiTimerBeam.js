5a52af3a5070499834d1ccd4879912698f0a5ae5
ivarmlee1 <ivarr5@gmail.com>
2017-03-24 02:41:24 -0700

    updating heroku

--- chatBots/cronKiwiTimer/cronKiwiTimerBeam.js
+++ chatBots/cronKiwiTimer/cronKiwiTimerBeam.js
@@ -11,7 +11,7 @@ module.exports = function(userId) {
   console.log(userId + ' running a beam cron timer for this guy')
   
   var beamJob = new CronJob({
-    cronTime: '* */5 * * *',
+    cronTime: '* */1 * * *',
     onTick: function() {
 
       console.log(userId + ' running a beam cron timer for this user every 5 mins')
