2846a5109c09cfb34f6a99a50e630fb10e8650fc
ivarmlee1 <ivarr5@gmail.com>
2017-03-27 13:58:34 -0700

    updating heroku

--- models/user.js
+++ models/user.js
@@ -15,16 +15,16 @@ module.exports = function(sequelize, DataTypes) {
     approvedThumb: DataTypes.BOOLEAN
   },{
       hooks: {
-        beforeCreate: function(createdUser, options, cb) {
-          console.log(createdUser)
-          console.log('created user')
-          // hash the password
-          var hash = bcrypt.hashSync(createdUser.password, 10)
-          // store the hash as the user's password
-          createdUser.password = hash
-          // continue to save the user, with no errors
-          cb(null, createdUser)
-        }
+        // beforeCreate: function(createdUser, options, cb) {
+        //   console.log(createdUser)
+        //   console.log('created user')
+        //   // hash the password
+        //   var hash = bcrypt.hashSync(createdUser.password, 10)
+        //   // store the hash as the user's password
+        //   createdUser.password = hash
+        //   // continue to save the user, with no errors
+        //   cb(null, createdUser)
+        // }
       },
       classMethods: {
         associate: function(models) {
@@ -36,17 +36,17 @@ module.exports = function(sequelize, DataTypes) {
         }
       },
       instanceMethods: {
-        validPassword: function(password) {
-          // return if the password matches the hash
-          return bcrypt.compareSync(password, this.password)
-        },
-        toJSON: function() {
-          // get the user's JSON data
-          var jsonUser = this.get()
-          // delete the password from the JSON data, and return
-          delete jsonUser.password
-          return jsonUser
-        }
+        // validPassword: function(password) {
+        //   // return if the password matches the hash
+        //   return bcrypt.compareSync(password, this.password)
+        // },
+        // toJSON: function() {
+        //   // get the user's JSON data
+        //   var jsonUser = this.get()
+        //   // delete the password from the JSON data, and return
+        //   delete jsonUser.password
+        //   return jsonUser
+        // }
       }
     })
   return user 
